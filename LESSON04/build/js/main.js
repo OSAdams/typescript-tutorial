efcb2cc7d5b1e49a09e0cc223414281299d10ee5
Owen Adams <falcocodes@osastack.dev>
2024-07-03 11:51:20 -0400

    LESSON04 Complete

--- LESSON04/build/js/main.js
+++ LESSON04/build/js/main.js
@@ -1,68 +1,79 @@
 "use strict";
-// LET VARIABLE VALUES ARE MUTABLE, CONST VALUES ARE NOT MUTABLE
-// ARRAYS
-let stringArr = ['eye', 'have', '10'];
-let guitars = ['Strat', 'Les Paul', 5150];
-let mixedData = ['EVH', 1984, true];
-// stringArr[0] = 42; -- type string array
-stringArr[0] = 'John'; // valid, value type string
-// stringArr.push(42) // number is not assignable to value type string
-guitars[0] = 1984; // valid, guitar index values can be a string or number
-guitars.unshift('Jim'); // valid, guitar index values can be a string or number
-// guitars.unshift(true) // invalid, guitars only accepts strings or numbers value
-// stringArr = guitars // invalid, guitars contains number values and stringArr only accepts string value types
-guitars = stringArr; // valid, guitars can contain string or number values
-let test = []; // type INFERRS any type value
-let bands = []; // type EXPLICITS string index values in the array
-bands.push('Van Halen'); // valid, van halen is a string type value
-// More strict, TUPLE. Define specific types and a specific array length
-let myTuple = ['Dave', 42, true]; // valid Tuple [string, number, boolean] value types
-let mixed = ['John', 1, false];
-// mixed = myTuple; // valid, index value types match and variable value type match
-// myTuple = mixed; // invalid, mixed is not required to have 3 elements in the array. It's a possibility that mixed may not have 3 elements
-// you cannot assign mixed to a tuple that only allows 3 index values.
-// OBJECTS
-let myObj; // valid, assigned value will be an object type. keep in mind - arrays are also an object;
-myObj = []; // valid
-console.log(typeof myObj); // an array is a typeof object in javascript
-myObj = bands; // valid, bands is an array value, array is typeof object
-myObj = {}; // valid
-const exampleObj = {
-    prop1: 'Owen',
-    prop2: true
+// Interface are Objects or Classes
+// Literal Types
+let myName; // myName value can only be 'Dave'
+let userName;
+userName = 'Amy'; // Must be 'Dave', 'John', or 'Amy'
+// DRI - DON'T REPEAT YOURSELF
+// functions  para type, para type, return type
+const add = (a, b) => a + b;
+const logMsg = (message) => console.log(message); // Void return type is for functions that do not return anything
+logMsg('Hello');
+logMsg(add(2, 3));
+// logMsg(add('a', 3)); // argument type string is not assignable to argument type number
+let subtract = function (c, d) {
+    return c - d;
 };
-let evh = {
-    name: 'Eddie', // must have name proptery with string value
-    active: false, // must have active proptery with boolean value
-    albums: [1984, 5150, 'OU812'] // must have alumbs array type with string or number type index values
+logMsg(subtract(4, 2));
+// interface mathFunction{
+//     (a: number, b: number): number;
+// }
+let multiply = function (c, d) {
+    return c * d;
 };
-let jp = {
-    name: 'Jimmy',
-    active: true,
-    albums: ['I', 'II', 'III']
-};
-// evh = jp // valid because all values meet type requirements defined in our type annotation
-let jimmeh = {
-    name: 'Jimmeh',
-    albums: [1942, 4219, 'hello world']
-};
-console.log('evh: ', evh);
-console.log('jp: ', jp);
-const greetGuitarist = (guitarist) => {
-    if (guitarist.name) {
-        return `Hello ${guitarist.name.toUpperCase()}!`;
+logMsg(multiply(1000000, 0.0034));
+// Optional Parameters must be THE LAST IN THE LIST
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
     }
-    return 'Hello!';
+    return a + b;
 };
-console.log(greetGuitarist(jp));
-// Enums
-// "Unlike most TypeScript features, Enums are not type-level addition to JavaScript but something added to the language and runtime."
-var Grade;
-(function (Grade) {
-    Grade[Grade["U"] = 24] = "U";
-    Grade[Grade["D"] = 25] = "D";
-    Grade[Grade["C"] = 26] = "C";
-    Grade[Grade["B"] = 27] = "B";
-    Grade[Grade["A"] = 28] = "A";
-})(Grade || (Grade = {}));
-console.log(Grade.U);
+const sumAll = (a, b, c = 2) => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+};
+logMsg(addAll(2, 3, 2));
+logMsg(sumAll(2, 3)); // 3rd parameter default value is 2, unless we assign a number value otherwise
+logMsg(sumAll(2, 3, 5));
+// Rest Parameters
+const total = (...nums) => {
+    return nums.reduce((prev, curr) => prev + curr);
+};
+logMsg(total(1, 2, 3, 4, 5));
+// never type - not used often
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
+// infinite loops will have a never type value
+const infinite = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        if (i > 100)
+            break; // without break statement, never type value to be returned. with break statement void type value to be returned
+    }
+};
+// custom type guard isNumber, isString
+const isNumber = (value) => {
+    return typeof value === 'number'
+        ? true
+        : false;
+};
+const isString = (value) => {
+    return typeof value === 'string'
+        ? true
+        : false;
+};
+// use of the never type
+const numberOrString = (value) => {
+    if (isString(value))
+        return 'string';
+    if (isNumber(value))
+        return 'number';
+    return createError('This should never happen!'); // createError returns a never type which will handle the possible error
+};
+logMsg(`numberOrString string: ${numberOrString('string')}`);
+logMsg(`numberOrString number: ${numberOrString(1998)}`);
--- LESSON04/src/main.ts
+++ LESSON04/src/main.ts
@@ -1,5 +1,113 @@
-interface Guitarist {
+// Type Aliases
+type stringOrNumber = string | number;
+
+type stringOrNumberArray = (string | number)[];
+
+type Guitarist = {
     name?: string,
     active: boolean,
-    albums: (string | number)[];
+    albums: stringOrNumberArray
 };
+
+type UserId = stringOrNumber;
+
+// Interface are Objects or Classes
+
+// Literal Types
+let myName: 'Dave'; // myName value can only be 'Dave'
+
+let userName: 'Dave' | 'John' | 'Amy';
+
+userName = 'Amy'; // Must be 'Dave', 'John', or 'Amy'
+
+// DRI - DON'T REPEAT YOURSELF
+
+// functions  para type, para type, return type
+const add = (a: number, b: number): number => a + b;
+
+const logMsg = (message: any): void => console.log(message); // Void return type is for functions that do not return anything
+
+logMsg('Hello');
+logMsg(add(2, 3));
+// logMsg(add('a', 3)); // argument type string is not assignable to argument type number
+
+let subtract = function (c: number, d: number): number {
+    return c - d;
+}
+
+logMsg(subtract(4, 2));
+
+type mathFunction = (a: number, b:number) => number
+// interface mathFunction{
+//     (a: number, b: number): number;
+// }
+
+let multiply: mathFunction = function(c, d) {
+    return c * d;
+}
+
+logMsg(multiply(1000000, 0.0034));
+
+// Optional Parameters must be THE LAST IN THE LIST
+const addAll = (a: number, b: number, c?: number): number => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+}
+
+const sumAll = (a: number, b: number, c: number = 2): number => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+}
+
+logMsg(addAll(2, 3, 2));
+logMsg(sumAll(2, 3)); // 3rd parameter default value is 2, unless we assign a number value otherwise
+logMsg(sumAll(2, 3, 5))
+
+// Rest Parameters
+const total = (...nums: number[]): number => {
+    return nums.reduce((prev, curr) => prev + curr);
+}
+
+logMsg(total(1, 2, 3, 4, 5))
+
+// never type - not used often
+
+const createError = (errMsg: string): never => {
+    throw new Error(errMsg);
+}
+
+// infinite loops will have a never type value
+const infinite = () => {
+    let i: number = 1;
+    while(true) {
+        i++
+        if (i > 100) break; // without break statement, never type value to be returned. with break statement void type value to be returned
+    }
+}
+
+// custom type guard isNumber, isString
+const isNumber = (value: any): boolean => {
+    return typeof value === 'number'
+        ? true
+        : false;
+}
+
+const isString = (value: any): boolean => {
+    return typeof value === 'string'
+        ? true
+        : false;
+}
+
+// use of the never type
+const numberOrString = (value: number | string): string => { // return can be string | undefined
+    if (isString(value)) return 'string'
+    if (isNumber(value)) return 'number'
+    return createError('This should never happen!'); // createError returns a never type which will handle the possible error
+}
+
+logMsg(`numberOrString string: ${numberOrString('string')}`);
+logMsg(`numberOrString number: ${numberOrString(1998)}`);
