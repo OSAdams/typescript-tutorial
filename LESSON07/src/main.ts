e9695fcde4040f5ffa351e1680918892ffc3476b
Owen Adams <falcocodes@osastack.dev>
2024-07-06 13:52:43 -0400

    loop through objects

--- LESSON07/src/main.ts
+++ LESSON07/src/main.ts
@@ -1,116 +1,61 @@
-class Coder {
-    publicLang!: string;
-    constructor(
-        // 'readonly' once the name is assigned, it cannot be changed
-        // 'private'  can only be accessed in the class
-        public readonly name: string, 
-        public music: string,
-        private age: number,
-        protected lang: string = 'TypeScript'
-    ) {
-        this.name = name;
-        this.music= music;
-        this.age = age;
-        this.lang = lang;
-    }
+// Index Signatures
 
-    public getAge() {
-        return `Hello, I'm ${this.age}`;
-    }
+interface TransactionObj {
+    readonly [index: string]: number
+    Pizza: number,
+    Books: number,
+    Job: number
 };
 
-const Dave = new Coder('Dave', 'Rock', 42, 'TypeScript');
-console.log(Dave.getAge());
-// console.log(Dave.age); // Property 'age' is private and only accessible within class 'Coder'.
-// console.log(Dave.lang); // Property 'lang' is protected and only accessible within class 'Coder' and its subclasses.
+// interface TransactionObj {
+//     readonly [index: string]: number // This is an index signature. keys will be a string, values will be a number
+// };
 
-class WebDev extends Coder {
-    constructor(
-        public computer: string,
-        name: string,
-        music: string,
-        age: number
-    ) {
-        super(name, music, age);
-        this.computer = computer;
-    }
+const todaysTransactions: TransactionObj = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50
+};
 
-    public getLang() {
-        return `I write ${this.lang}`;
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions['Pizza']);
+
+// Dynamic access
+// Without a loop
+
+let prop: string = 'Pizza';
+console.log(todaysTransactions[prop]);
+
+// With loop
+const todaysNet = (transactions: TransactionObj): number => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction];
     }
+    return total;
+};
+
+console.log(todaysNet(todaysTransactions))
+
+// todaysTransactions.Pizza = 40; // Index signature in type 'TransactionObj' only permits reading
+
+console.log(todaysTransactions['Dave']);
+
+interface Student {
+    // [key: string]: string | number | number[] | undefined
+    name: string,
+    GPA: number,
+    classes?: number[]
+};
+
+const student: Student = {
+    name: "Doug",
+    GPA: 3.5,
+    classes: [100, 200]
 }
 
-const Sara = new WebDev('Mac', 'Sara', 'LoFi', 25);
-console.log(Sara.getLang());
+// console.log(student);
 
-// Implementing and interface to a class
-
-interface Musician {
-    name: string, 
-    instrument: string,
-    play(action: string): string
-}
-
-class Guitarist implements Musician {
-    name: string
-    instrument: string
-    constructor(name: string, instrument: string) {
-        this.name = name;
-        this.instrument = instrument;
-    }
-
-    play(action: string) {
-        return `${this.name} ${action} the ${this.instrument}`;
-    }
-}
-
-const Page = new Guitarist('Jimmy', 'Guitar');
-console.log(Page.play('strums'));
-
-// 
-
-class Peeps {
-    static count: number = 0 // 'static' count applies to the class directly itself. we do not use 'this'
-    static getCount(): number {
-        return Peeps.count;
-    }
-    public id: number;
-    constructor(public name: string) {
-        this.name = name;
-        this.id = ++Peeps.count; // ++ on left will increment before assignment. ++ on right will increment after assignment
-    }
-}
-
-const John = new Peeps('John');
-const Steve = new Peeps('Steve');
-const Amy = new Peeps('Amy');
-
-console.log(Amy.id);
-console.log(Steve.id);
-console.log(Peeps.count);
-
-// Getters and Setters
-
-class Bands {
-    private dataState: string[]
-
-    constructor() {
-        this.dataState = [];
-    }
-
-    public get data(): string[] { // readonly because there isn't a setter
-        return this.dataState;
-    }
-
-    public set data(value: string[]) {
-        if (Array.isArray(value) && value.every(el => typeof el === 'string')) {
-            this.dataState = value;
-            return
-        } else throw new Error('Param is not an array of strings');
-    }
- }
-
- const MyBands = new Bands();
- MyBands.data = ['Neil Young', 'Led Zep'];
- MyBands.data = [...MyBands.data, 'ZZ Top'];
- console.log(MyBands.data);
+for (const key in student) {
+    console.log(`${key}: ${student[key as keyof Student]}`) // keyof creates a union type
+};
