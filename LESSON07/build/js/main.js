fada0556f749680efa4b0706ad7d81297cf5d2b1
Owen Adams <falcocodes@osastack.dev>
2024-07-06 14:01:06 -0400

    LESSON07 complete

--- LESSON07/build/js/main.js
+++ LESSON07/build/js/main.js
@@ -37,3 +37,18 @@ for (const key in student) {
     console.log(`${key}: ${student[key]}`); // keyof creates a union type
 }
 ;
+Object.keys(student).map(key => {
+    console.log(student[key]); // if you don't know the object type, keyof typeof student
+});
+const logStudentKey = (student, key) => {
+    console.log(`Student ${key}: ${student[key]}`);
+};
+logStudentKey(student, 'name');
+const monthlyIncomes = {
+    salary: 500,
+    bonus: 100,
+    sidehustle: 250
+};
+for (const revenue in monthlyIncomes) {
+    console.log(monthlyIncomes[revenue]);
+}
--- LESSON07/src/main.ts
+++ LESSON07/src/main.ts
@@ -59,3 +59,34 @@ const student: Student = {
 for (const key in student) {
     console.log(`${key}: ${student[key as keyof Student]}`) // keyof creates a union type
 };
+
+Object.keys(student).map(key => {
+    console.log(student[key as keyof typeof student]); // if you don't know the object type, keyof typeof student
+});
+
+const logStudentKey = (student: Student, key: keyof Student): void => {
+    console.log(`Student ${key}: ${student[key]}`);
+}
+
+logStudentKey(student, 'name');
+
+/////////////////////////////////////////
+
+// interface Incomes {
+//     [key: string]: number
+//     Pizza: string // invalid
+// };
+
+type Streams = 'salary' | 'bonus' | 'sidehustle';
+
+type Incomes = Record<Streams, number | string>; // record utility type
+
+const monthlyIncomes: Incomes = {
+    salary: 500,
+    bonus: 100,
+    sidehustle: 250
+};
+
+for (const revenue in monthlyIncomes) {
+    console.log(monthlyIncomes[revenue as keyof Incomes])
+}
