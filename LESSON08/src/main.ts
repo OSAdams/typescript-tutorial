67db4aebecfa7146b371878e76568ce53c72f66c
Owen Adams <falcocodes@osastack.dev>
2024-07-13 13:48:11 -0400

    isObj function definition and test cases

--- LESSON08/src/main.ts
+++ LESSON08/src/main.ts
@@ -1,92 +1,14 @@
-// Index Signatures
+// Generics
 
-interface TransactionObj {
-    readonly [index: string]: number
-    Pizza: number,
-    Books: number,
-    Job: number
-};
+const stringEcho = (arg: string): string => arg;
+const echo = <T>(arg: T): T => arg;
 
-// interface TransactionObj {
-//     readonly [index: string]: number // This is an index signature. keys will be a string, values will be a number
-// };
-
-const todaysTransactions: TransactionObj = {
-    Pizza: -10,
-    Books: -5,
-    Job: 50
-};
-
-console.log(todaysTransactions.Pizza);
-console.log(todaysTransactions['Pizza']);
-
-// Dynamic access
-// Without a loop
-
-let prop: string = 'Pizza';
-console.log(todaysTransactions[prop]);
-
-// With loop
-const todaysNet = (transactions: TransactionObj): number => {
-    let total = 0;
-    for (const transaction in transactions) {
-        total += transactions[transaction];
-    }
-    return total;
-};
-
-console.log(todaysNet(todaysTransactions))
-
-// todaysTransactions.Pizza = 40; // Index signature in type 'TransactionObj' only permits reading
-
-console.log(todaysTransactions['Dave']);
-
-interface Student {
-    // [key: string]: string | number | number[] | undefined
-    name: string,
-    GPA: number,
-    classes?: number[]
-};
-
-const student: Student = {
-    name: "Doug",
-    GPA: 3.5,
-    classes: [100, 200]
+const isObj = <T>(arg: T): boolean => {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null);
 }
 
-// console.log(student);
-
-for (const key in student) {
-    console.log(`${key}: ${student[key as keyof Student]}`) // keyof creates a union type
-};
-
-Object.keys(student).map(key => {
-    console.log(student[key as keyof typeof student]); // if you don't know the object type, keyof typeof student
-});
-
-const logStudentKey = (student: Student, key: keyof Student): void => {
-    console.log(`Student ${key}: ${student[key]}`);
-}
-
-logStudentKey(student, 'name');
-
-/////////////////////////////////////////
-
-// interface Incomes {
-//     [key: string]: number
-//     Pizza: string // invalid
-// };
-
-type Streams = 'salary' | 'bonus' | 'sidehustle';
-
-type Incomes = Record<Streams, number | string>; // record utility type
-
-const monthlyIncomes: Incomes = {
-    salary: 500,
-    bonus: 100,
-    sidehustle: 250
-};
-
-for (const revenue in monthlyIncomes) {
-    console.log(monthlyIncomes[revenue as keyof Incomes])
-}
+console.log('true value', isObj(true));
+console.log('"John"', isObj('John'));
+console.log('[1, 2, 3]', isObj([1, 2, 3]));
+console.log('{ name: "John" }', isObj({ name: 'John' }));
+console.log('null value', isObj(null));
