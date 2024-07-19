2d28f1d232db02fa5adce8f78a0c500d2ac0f3b0
Owen Adams <falcocodes@osastack.dev>
2024-07-19 10:35:03 -0400

    complete lesson 14

--- LESSON14/lesson14/src/Counter.tsx
+++ LESSON14/lesson14/src/Counter.tsx
@@ -1,14 +1,16 @@
-import { ReactNode, useReducer } from "react";
+import { ChangeEvent, ReactNode, useReducer } from "react";
 
-const initState = { count: 0 };
+const initState = { count: 0, text: '' };
 
 const enum REDUCER_ACTION_TYPE {
     INCREMENT,
-    DECREMENT
+    DECREMENT,
+    NEW_INPUT
 }
 
 type ReducerAction = {
-    type: REDUCER_ACTION_TYPE
+    type: REDUCER_ACTION_TYPE,
+    payload?: string
 }
 
 const reducer = (state: typeof initState, action: ReducerAction): typeof initState => {
@@ -17,6 +19,8 @@ const reducer = (state: typeof initState, action: ReducerAction): typeof initSta
             return { ...state, count: state.count + 1 }
         case REDUCER_ACTION_TYPE.DECREMENT:
             return { ...state, count: state.count - 1 }
+        case REDUCER_ACTION_TYPE.NEW_INPUT:
+            return { ...state, text: action.payload ?? '' }
         default: 
             throw new Error()
     }
@@ -32,6 +36,9 @@ const Counter = ({ children }: ChildrenType) => {
 
     const increment = () => dispatch({ type: REDUCER_ACTION_TYPE.INCREMENT })
     const decrement = () => dispatch({ type: REDUCER_ACTION_TYPE.DECREMENT })
+    const handleTextInput = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({ type: REDUCER_ACTION_TYPE.NEW_INPUT, payload: e.target.value })
+    }
 
     return (
         <>
@@ -40,6 +47,8 @@ const Counter = ({ children }: ChildrenType) => {
                 <button onClick={ increment }>+</button>
                 <button onClick={ decrement }>-</button>
             </div>
+            <input type="text" onChange={ handleTextInput } />
+            <h2>{ state.text }</h2>
         </>
     )
 }
